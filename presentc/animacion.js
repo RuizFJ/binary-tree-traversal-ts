81f60a17799114d57c6ce03259b271419fba3ce7
Chris <chrisreyesoc@gmail.com>
2025-10-27 21:46:35 -0600

    Modificacion presentacion

--- presentc/animacion.js
+++ presentc/animacion.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const slide = document.querySelector(".slide");
 
-  //movimiento del raton
+  //movimiento del cuadro
   document.addEventListener("mousemove", (e) => {
     const x = (e.clientX / window.innerWidth - 0.5) * 2;
     const y = (e.clientY / window.innerHeight - 0.5) * 2;
 
+    //efecto de slide al mostrar
     slide.style.transform = `rotateX(${-y * 5}deg) rotateY(${
       x * 5
     }deg) scale(1.05)`;
